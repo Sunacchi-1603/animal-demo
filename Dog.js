6a055c29985852ddfb85de62eb69607e325cfea5
Sunacchi-1603 <sunachi1603@gmail.com>
2022-03-20 15:50:33 +0900

    add dog module

--- Dog.js
+++ Dog.js
@@ -1,6 +1,13 @@
-function Dong() {
+import chalk from 'chalk';
+function Dog(name) {
 	this.stomach = [];
-}
+	this.name = name;
+};
+
 Dog.prototype.eat = function(cat) {
-	this.stomach.push(cat)
+	this.stomach.push(cat);
+};
+Dog.prototype.sayHi = function() {
+	console.log('hi my name is '  + chalk.blue( this.name));
 }
+export { Dog};
---
+++ index.js
@@ -0,0 +1,13 @@
+import { Dog } from './Dog.js';
+import { Cat } from './cat.js';
+import { Mouse } from './mouse.js';
+
+// var dog = new Dog('pluto');
+// //dog.sayHi();
+// var tom = new Cat()
+// dog.eat(tom);
+// console.log(dog);
+
+
+var dog = new Dog('tom');
+dog.sayHi();
